1fbe0199547d1222856afd4a8f7114159f628d5d
jeongeun-ni <208091286+YJKor@users.noreply.github.com>
2025-07-16 12:40:59 +0900

    12:40 수정

---
+++ Assets/03_Scripts/ButtonTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    
+
+    public void Test()
+    {
+        Debug.Log("Button clicked!");
+    }
+}
